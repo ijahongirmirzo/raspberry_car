8c24dab4e76c4d38e60ae0610c1b9ac0ab561829
Jahongirmirzo <ijahongirmirzo@gmail.com>
2021-03-20 21:55:31 +0500

    Bismillah

--- IR_sensor_2.py
+++ IR_sensor_2.py
@@ -6,9 +6,9 @@ while True:
     distance = get_distance()
     if distance > 50:
         forward()
-    elif distance < 50:
+    if distance < 50:
         backward()
-    elif distance == 50:
+    if distance == 50:
         stop()
         time.sleep(0.5)
 
