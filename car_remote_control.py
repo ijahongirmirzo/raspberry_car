6bdde151c5f22dd9060e331e23672ee9868ecdee
Jahongirmirzo <ijahongirmirzo@gmail.com>
2021-05-27 17:56:01 +0500

    Bismillah

--- car_remote_control.py
+++ car_remote_control.py
@@ -1,6 +1,5 @@
 from pynput import keyboard
-from car_details import *
-import time
+from car_control import *
 
 keys = keyboard.Key
 PRESSED_KEYS_COUNT = 0
@@ -10,32 +9,21 @@ def on_press(key):
     if not hasattr(globals, 'PRESSED_KEYS_COUNT'):
         print('initialized on press')
         global PRESSED_KEYS_COUNT
-        PRESSED_KEYS_COUNT = 0
     print("%s is pressed" % key)
-    if key in [keys.up, keys.down, keys.right, keys.left]:
-        PRESSED_KEYS_COUNT = PRESSED_KEYS_COUNT + 1
-        print("release count %s" % PRESSED_KEYS_COUNT)
-        if key == keys.up:
-            return forward()
-        if key == keys.down:
-            return backward()
-        if key == keys.right:
-            return right()
-        if key == keys.left:
-            return left()
+    if key == keys.up:
+        return forward()
+    if key == keys.down:
+        return backward()
+    if key == keys.right:
+        return right()
+    if key == keys.left:
+        return left()
 
 
 def on_release(key):
-    if not hasattr(globals, 'PRESSED_KEYS_COUNT'):
-        print('initialized on release')
-        global PRESSED_KEYS_COUNT
-        PRESSED_KEYS_COUNT = 0
     print("%s is released" % key)
     if key in [keys.up, keys.down, keys.right, keys.left]:
-        print("release count %s" % PRESSED_KEYS_COUNT)
-        PRESSED_KEYS_COUNT = PRESSED_KEYS_COUNT - 1
-        if PRESSED_KEYS_COUNT <= 0:
-            stop()
+        destroy()
 
 
 if __name__ == '__main__':
