f0edd942f35891331655726ca74cecad6b1d61da
Jahongirmirzo <ijahongirmirzo@gmail.com>
2021-05-27 18:15:55 +0500

    Bismillah

--- car_remote_control.py
+++ car_remote_control.py
@@ -1,31 +1,50 @@
 from pynput import keyboard
-from car_control import *
+from car_details import *
+import time
 
 keys = keyboard.Key
-
+PRESSED_KEYS_COUNT = 0
 
 def on_press(key):
-    setup()
-    print("%s is pressed" % key)
-    if key == keys.up:
-        return forward()
-    if key == keys.down:
-        return backward()
-    if key == keys.right:
-        return right()
-    if key == keys.left:
-        return left()
+    if not hasattr(globals, 'PRESSED_KEYS_COUNT'):
+        print('initialized on press')
+        global PRESSED_KEYS_COUNT
+        PRESSED_KEYS_COUNT = 0
+    print ("%s is pressed" % key)
+    if key in [keys.up, keys.down, keys.right, keys.left]:
+        PRESSED_KEYS_COUNT = PRESSED_KEYS_COUNT + 1
+        print("release count %s" % PRESSED_KEYS_COUNT)
+        if key == keys.up:
+            return forward()
+        if key == keys.down:
+            return backward()
+        if key == keys.right:
+            return right()
+        if key == keys.left:
+            return left()
+
 
 
 def on_release(key):
-    print("%s is released" % key)
+    if not hasattr(globals, 'PRESSED_KEYS_COUNT'):
+        print('initialized on release')
+        global PRESSED_KEYS_COUNT
+        PRESSED_KEYS_COUNT = 0
+    print ("%s is released" % key)
     if key in [keys.up, keys.down, keys.right, keys.left]:
-        destroy()
+        print("release count %s" % PRESSED_KEYS_COUNT)
+        PRESSED_KEYS_COUNT = PRESSED_KEYS_COUNT - 1
+        if PRESSED_KEYS_COUNT <= 0:
+            stop()
 
 
 if __name__ == '__main__':
+    pressed_keys_count = 0
     setup()
+    # while True:
+    #     print(get_distance())
+    #     time.sleep(0.5)
     with keyboard.Listener(
-            on_press=on_press,
-            on_release=on_release) as listener:
+        on_press=on_press,
+        on_release=on_release) as listener:
         listener.join()
