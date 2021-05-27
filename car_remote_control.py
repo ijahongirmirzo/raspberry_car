da22479b2d42dc7bdf826a8faea538de70f5d7f5
Jahongirmirzo <ijahongirmirzo@gmail.com>
2021-05-27 18:00:57 +0500

    Bismillah

--- car_remote_control.py
+++ car_remote_control.py
@@ -2,13 +2,9 @@ from pynput import keyboard
 from car_control import *
 
 keys = keyboard.Key
-PRESSED_KEYS_COUNT = 0
 
 
 def on_press(key):
-    if not hasattr(globals, 'PRESSED_KEYS_COUNT'):
-        print('initialized on press')
-        global PRESSED_KEYS_COUNT
     print("%s is pressed" % key)
     if key == keys.up:
         return forward()
@@ -27,11 +23,7 @@ def on_release(key):
 
 
 if __name__ == '__main__':
-    pressed_keys_count = 0
     setup()
-    # while True:
-    #     print(get_distance())
-    #     time.sleep(0.5)
     with keyboard.Listener(
             on_press=on_press,
             on_release=on_release) as listener:
