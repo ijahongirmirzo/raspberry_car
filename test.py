591e65f27f0c3f56d56812973ed912ca06b01130
Jahongirmirzo <ijahongirmirzo@gmail.com>
2021-05-28 08:48:46 +0500

    t

--- test.py
+++ test.py
@@ -4,14 +4,22 @@ from car_details import *
 
 setup()
 not_found_times = 0
+slowness = 1.5
+detection_count = 0
 while True:
     trace = get_trace()
     if trace == RIGHT:
+        detection_count += 1
         right()
     elif trace == LEFT:
+        detection_count += 1
         left()
     elif trace == STRAIGHT:
-        forward(2.5)
+        if detection_count != 0:
+            detection_count -= 1
+        forward(slowness)
     elif trace == STOP:
         stop()
-    time.sleep(0.3)
+    if detection_count >= 2:
+        slowness = 5
+    # time.sleep(0.3)
