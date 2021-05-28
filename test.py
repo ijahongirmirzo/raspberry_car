b454216c4fbb1b52851f3a64e0465622f4d8ca7a
Jahongirmirzo <ijahongirmirzo@gmail.com>
2021-05-28 21:27:49 +0500

    t

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from car_details import *
 
 setup()
 not_found_times = 0
-slowness = 1.5
+slowness = 1.2
 detection_count = 0
 stop_skip_counter = 0
 while True:
@@ -19,7 +19,7 @@ while True:
         if detection_count != 0:
             detection_count -= 1
         else:
-            slowness = 1.5
+            slowness = 1.2
         forward(slowness)
     elif trace == STOP:
         forward()
