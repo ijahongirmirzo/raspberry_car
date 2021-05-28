a6eb45e6253c96e7085b2b56d2008be6db02376a
Jahongirmirzo <ijahongirmirzo@gmail.com>
2021-05-28 23:01:01 +0500

    t

--- test.py
+++ test.py
@@ -9,7 +9,6 @@ detection_count = 0
 stop_skip_counter = 0
 while True:
     trace = get_trace()
-    obstacle = get_obstacle()
     if trace == RIGHT:
         detection_count += 1
         right()
@@ -27,4 +26,33 @@ while True:
         time.sleep(1.5)
     if detection_count >= 2:
         slowness = 2
+
+    obstacle = get_obstacle()
+    if obstacle == STOP:
+        right()
+        time.sleep(1.7)
+        while True:
+            forward(2)
+            trace = get_trace()
+            if trace == STOP:
+                left()
+                time.sleep(1.7)
+                obstacle = get_obstacle()
+                if obstacle == STRAIGHT:
+                    forward(2)
+                    while True:
+                        trace = get_trace()
+                        if trace == STOP:
+                            left()
+                            time.sleep(1.7)
+                            forward()
+                            while True:
+                                trace = get_trace()
+                                if trace in [STOP, LEFT, RIGHT]:
+                                    right()
+                                    time.sleep(1.7)
+                                    forward()
+                                    break
+                            break
+                    break
     # time.sleep(0.3)
