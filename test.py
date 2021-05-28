71b5fb8e7122324a368981387f4467d68bc5c5c5
Jahongirmirzo <ijahongirmirzo@gmail.com>
2021-05-28 11:49:21 +0500

    t

--- test.py
+++ test.py
@@ -7,21 +7,22 @@ not_found_times = 0
 slowness = 1
 detection_count = 0
 while True:
-    trace = get_trace()
-    if trace == RIGHT:
-        detection_count += 1
-        right()
-    elif trace == LEFT:
-        detection_count += 1
-        left()
-    elif trace == STRAIGHT:
-        if detection_count != 0:
-            detection_count -= 1
-        else:
-            slowness = 1
-        forward(slowness)
-    elif trace == STOP:
-        stop()
-    if detection_count >= 2:
-        slowness = 2
-    time.sleep(0.3)
+    forward(2)
+    # trace = get_trace()
+    # if trace == RIGHT:
+    #     detection_count += 1
+    #     right()
+    # elif trace == LEFT:
+    #     detection_count += 1
+    #     left()
+    # elif trace == STRAIGHT:
+    #     if detection_count != 0:
+    #         detection_count -= 1
+    #     else:
+    #         slowness = 1
+    #     forward(slowness)
+    # elif trace == STOP:
+    #     stop()
+    # if detection_count >= 2:
+    #     slowness = 2
+    # time.sleep(0.3)
