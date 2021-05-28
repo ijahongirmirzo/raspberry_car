af7a0fcf8d4f505d2cadee25014739bca2df91d1
Jahongirmirzo <ijahongirmirzo@gmail.com>
2021-05-28 07:21:16 +0500

    Bismillah

--- test.py
+++ test.py
@@ -1,10 +1,23 @@
 import time
 
-from car_details import forward, stop, setup, smooth_left
+from car_details import *
 
+trace = get_trace()
 setup()
-forward()
-time.sleep(5)
-smooth_left()
-time.sleep(2)
-stop()
+not_found_times = 0
+while True:
+    if trace == RIGHT:
+        not_found_times = 0
+        smooth_right()
+    elif trace == LEFT:
+        not_found_times = 0
+        smooth_left()
+    elif trace == STRAIGHT:
+        not_found_times = 0
+        forward(2)
+    elif trace == STOP:
+        if not_found_times <= 5:
+            forward(3)
+            not_found_times += 1
+        else:
+            stop()
