7f2b1e9e756851bfefa3221f7e5e729184e8778a
Jahongirmirzo <ijahongirmirzo@gmail.com>
2021-05-28 06:52:31 +0500

    Bismillah

--- car_remote_control.py
+++ car_remote_control.py
@@ -8,45 +8,31 @@ PRESSED_KEYS_COUNT = 0
 
 
 def on_press(key):
-    if not hasattr(globals, 'PRESSED_KEYS_COUNT'):
-        print('initialized on press')
-        global PRESSED_KEYS_COUNT
-        PRESSED_KEYS_COUNT = 0
     print("%s is pressed" % key)
-    if key in [keys.up, keys.down, keys.right, keys.left]:
-        PRESSED_KEYS_COUNT = PRESSED_KEYS_COUNT + 1
-        print("release count %s" % PRESSED_KEYS_COUNT)
-        if key == keys.up:
-            return forward(slowness_time=10)
-            # trace = get_trace()
-            # if trace == RIGHT:
-            #     return right()
-            # elif trace == LEFT:
-            #     return left()
-            # elif trace == STOP:
-            #     return stop()
-            # elif trace == STRAIGHT:
-            #     return forward()
-            # return forward()
-        if key == keys.down:
-            return backward()
-        if key == keys.right:
-            return right()
-        if key == keys.left:
-            return smooth_left()
+    if key == keys.up:
+        return forward(slowness_time=10)
+        # trace = get_trace()
+        # if trace == RIGHT:
+        #     return right()
+        # elif trace == LEFT:
+        #     return left()
+        # elif trace == STOP:
+        #     return stop()
+        # elif trace == STRAIGHT:
+        #     return forward()
+        # return forward()
+    if key == keys.down:
+        return backward()
+    if key == keys.right:
+        return right()
+    if key == keys.left:
+        return left()
 
 
 def on_release(key):
-    if not hasattr(globals, 'PRESSED_KEYS_COUNT'):
-        print('initialized on release')
-        global PRESSED_KEYS_COUNT
-        PRESSED_KEYS_COUNT = 0
     print("%s is released" % key)
     if key in [keys.up, keys.down, keys.right, keys.left]:
-        print("release count %s" % PRESSED_KEYS_COUNT)
-        PRESSED_KEYS_COUNT = PRESSED_KEYS_COUNT - 1
-        if PRESSED_KEYS_COUNT <= 0:
-            stop()
+        stop()
 
 
 if __name__ == '__main__':
