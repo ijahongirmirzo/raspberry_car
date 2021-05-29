b872e49246770a1b5393d619cf0b3f36ddd8f084
Jahongirmirzo <ijahongirmirzo@gmail.com>
2021-05-29 19:21:08 +0500

    1

--- utils.py
+++ utils.py
@@ -9,4 +9,9 @@ def angle_to_time(angle):
 def cm_to_time(cm):
     # ensure speed is 50%
     a_cm = 0.8
-    return a_cm * cm
+    if cm == 1:
+        return 0.8
+    elif cm == 2:
+        return 0.16
+    elif cm == 4:
+        return 0.32
