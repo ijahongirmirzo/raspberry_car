ad5038841bf514400e7961f2095a46ca1dc4d18e
Jahongirmirzo <ijahongirmirzo@gmail.com>
2021-05-30 13:04:28 +0500

    Bismillah

--- 2midterm.py
+++ 2midterm.py
@@ -107,8 +107,8 @@ while True:
         ultra_obstacle = car.get_distance()
         if ultra_obstacle <= 20:
             car.stop()
-            time.sleep(1)
             while True:
+                time.sleep(1)
                 ultra_obstacle = car.get_distance()
                 if ultra_obstacle > 20:
                     print(ultra_obstacle)
